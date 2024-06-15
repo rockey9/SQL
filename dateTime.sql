e4f3a7027914de03acc251bfba5445d526bc9a90
rockey9 <48086335+rockey9@users.noreply.github.com>
2024-06-15 22:12:05 +0530

    Add files via upload

---
+++ dateTime.sql
@@ -0,0 +1,157 @@
+select sysdate from dual;
+select systimestamp from dual;
+
+select sysdate,to_char(sysdate,'CC') from dual;
+
+select sysdate,to_char(sysdate,'SCC') from dual;
+
+select sysdate,to_char(sysdate,'D') from dual;
+
+select sysdate,to_char(sysdate,'DD') from dual;
+
+select sysdate,to_char(sysdate,'DDD') from dual;
+
+select sysdate,to_char(sysdate,'Day') from dual;
+
+select sysdate,to_char(sysdate,'day') from dual;
+
+select sysdate,to_char(sysdate,'DAY') from dual;
+
+select sysdate,to_char(sysdate,'DL') from dual;
+
+select sysdate,to_char(sysdate,'DS') from dual;
+
+select sysdate,to_char(sysdate,'DY') from dual;
+
+select sysdate,to_char(sysdate,'E') from dual;
+
+select sysdate,to_char(sysdate,'EE') from dual;
+
+select sysdate,to_char(sysdate,'FF') from dual;
+
+select systimestamp,to_char(systimestamp,'FF') from dual;
+
+SELECT systimestamp,to_char(SYSTIMESTAMP, 'SS.FF3') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'FM') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'FM') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'FX') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'FX') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'HH') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'HH') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'HH12') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'HH12') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'HH24') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'HH24') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'IW') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'IW') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'IYYY') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'IYYY') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'IYY') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'IYY') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'IY') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'IY') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'I') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'I') from DUAL;
+
+SELECT sysdate,to_char(sysdate, 'J') from DUAL;
+SELECT systimestamp,to_char(systimestamp, 'J') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MI') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MI') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MM') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MM') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MON') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MON') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MONTH') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MONTH') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'mon') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'mon') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'month') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'month') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MON') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MON') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'Q') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'Q') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MON') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MON') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MONTH') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MONTH') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'MON') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'MON') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'RM') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'RM') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'RR') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'RR') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'RRRR') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'RRRR') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'SS') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'SS') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'SSSS') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'SSSS') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'TS') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'TS') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'TZD') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'TZD') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'TZH') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'TZH') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'TZM') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'TZM') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'TZR') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'TZR') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'WW') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'WW') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'W') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'W') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'X') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'X') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'Y,YYY') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'Y,YYY') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'YEAR') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'YEAR') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'SYEAR') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'SYEAR') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'YYY') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'YYY') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'YY') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'YY') from DUAL;
+
+SELECT sysdate,TO_CHAR(sysdate, 'Y') from DUAL;
+SELECT systimestamp,TO_CHAR(systimestamp, 'Y') from DUAL;
